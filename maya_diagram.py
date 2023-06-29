737aee836fa93566c03df8d115fb7dc1e1e14173
FoodieFridays <61214708+FoodieFridays@users.noreply.github.com>
2023-06-29 16:50:08 -0400

    Maya diagram support
    Added support for generating a Maya diagram given a partition and charge, and for returning the partition when given a Maya diagram and charge.

---
+++ maya_diagram.py
@@ -0,0 +1,107 @@
+from random import randint
+
+import matplotlib.pyplot as plt
+
+import asyncio
+from pyodide import create_proxy
+import numpy as np
+
+
+def Setup_Button_Listeners():
+    btnList = document.querySelectorAll(".button")
+    for i in range(len(btnList)):
+        e = document.getElementById(btnList[i].id)
+        btn_event = create_proxy(Process_Button)
+        e.addEventListener("click", btn_event)
+
+
+def get_integer_partitions(n):
+    full_list = []
+
+    # Using a recursive approach
+    def sub_partition(n, k, prefix):
+        if n == 0:
+            full_list.append(prefix)
+            return
+        if k > n:
+            return
+        sub_partition(n, k + 1, prefix)
+        sub_partition(n - k, k, prefix + [k])
+
+    sub_partition(n, 1, [])
+    return full_list
+
+
+async def Process_Button(event):
+    if document.getElementById("evtMsg").innerHTML == '100':  # button plot_it
+        fig = await plot_it()
+        # pyscript.write('lineplot', fig)
+
+
+async def plot_it(*args, **kwargs):
+    output_box = Element('test-output')
+    output_box_2 = Element('test-output-2')
+
+    text = Element('test-input').element.value
+    charge = int(Element('test-input-2').element.value)
+
+    str_arr = np.array(text.split(","))
+    partition = []
+
+    for num in str_arr:
+        partition.append(int(num))
+
+    partition.sort(reverse=True)
+
+    # Various sets defined in Nathan's paper
+    s_plus = []
+    s_minus = []
+    maya_output = []
+
+    # Numbers to go in s_plus
+    for i in range(len(partition)):
+        if partition[i] > i:
+            s_plus.append(partition[i] - i)
+        else:
+            break
+
+    # Numbers to go into s_minus
+    for i in range(1, len(partition)):
+        if sum(num >= i for num in partition) - i >= 1:
+            s_minus.append((sum(num >= i for num in partition) - i) * -1)
+        else:
+            break
+
+    # Numbers to go into s_plus
+    for num in s_plus:
+        maya_output.append(num)
+
+    # Adding 0 into the final output
+    maya_output.append(0)
+
+    # Appending to the final output, plus a few more at the end
+    for i in range(-1, min(s_minus) - 3, -1):
+        if i not in s_minus:
+            maya_output.append(i)
+
+    # Adjusting for the charge, if needed
+    if charge != 0:
+        for i in range(len(maya_output)):
+            maya_output[i] += charge
+
+    # Creating the visual diagram
+    visual = ""
+    for i in range(max(maya_output), min(maya_output) - 3, -1):
+        if i in s_plus or i in s_minus:
+            visual += "● "
+        else:
+            visual += "○ "
+
+    # Appending the visual diagram to the final output
+    maya_output.append(visual + "...")
+
+    output_box.write("Numerical Diagram: " + str(maya_output[:len(maya_output) - 1]))
+    output_box_2.write("Visual Diagram: " + maya_output[-1])
+
+
+Setup_Button_Listeners()
---
+++ partition_from_maya.py
@@ -0,0 +1,99 @@
+from random import randint
+
+import matplotlib.pyplot as plt
+
+import asyncio
+from pyodide import create_proxy
+import numpy as np
+
+
+def Setup_Button_Listeners():
+    btnList = document.querySelectorAll(".button")
+    for i in range(len(btnList)):
+        e = document.getElementById(btnList[i].id)
+        btn_event = create_proxy(Process_Button)
+        e.addEventListener("click", btn_event)
+
+
+def get_integer_partitions(n):
+    full_list = []
+
+    # Using a recursive approach
+    def sub_partition(n, k, prefix):
+        if n == 0:
+            full_list.append(prefix)
+            return
+        if k > n:
+            return
+        sub_partition(n, k + 1, prefix)
+        sub_partition(n - k, k, prefix + [k])
+
+    sub_partition(n, 1, [])
+    return full_list
+
+
+async def Process_Button(event):
+    if document.getElementById("evtMsg").innerHTML == '100':  # button plot_it
+        fig = await plot_it()
+        # pyscript.write('lineplot', fig)
+
+
+async def plot_it(*args, **kwargs):
+    output_box = Element('test-output')
+
+    text = Element('test-input').element.value
+    charge = int(Element('test-input-2').element.value)
+
+    str_arr = np.array(text.split(","))
+    maya_set = []
+
+    for num in str_arr:
+        maya_set.append(int(num))
+
+    # Various sets defined in Nathan's paper
+    s_plus = []
+    s_minus = []
+
+    # A "helper" set
+    s_minus_adjust = []
+
+    # Final output
+    partition = []
+
+    # Adjust for a charge, if needed
+    if charge != 0:
+        for i in range(len(maya_set)):
+            maya_set[i] -= charge
+
+    # Numbers to go into s_plus
+    for i in range(len(maya_set)):
+        if maya_set[i] != 0:
+            s_plus.append(maya_set[i])
+        else:
+            break
+
+    # Adjusting numbers from s_plus to go into the partition
+    for i in range(len(s_plus)):
+        partition.append(s_plus[i] + i)
+
+    # Numbers to go into s_minus
+    for i in range(-1, min(maya_set) - 1, -1):
+        if i not in maya_set:
+            s_minus.append(i)
+
+    # Numbers to go into the s_minus adjusted set
+    for num in s_minus:
+        s_minus_adjust.append(-1 * num)
+
+    for i in range(len(s_minus_adjust)):
+        s_minus_adjust[i] -= i
+
+    # Based on s_minus_adjusted, we compute the additional numbers that must be added to the partition
+    for i in range(1, max(s_minus_adjust) + 1):
+        count = sum(num >= i for num in s_minus_adjust)
+        partition.append(count)
+
+    output_box.write(partition)
+
+
+Setup_Button_Listeners()
