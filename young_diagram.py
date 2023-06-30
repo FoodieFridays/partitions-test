179b89f12c40f11c6d6eb7c462e6bbc70d52be75
FoodieFridays <61214708+FoodieFridays@users.noreply.github.com>
2023-06-30 16:31:26 -0400

    Added matplotlib support for Young diagrams

--- young_diagram.py
+++ young_diagram.py
@@ -1,17 +1,18 @@
 from random import randint
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 import asyncio
 from pyodide import create_proxy
 import numpy as np
 
 
-def Setup_Button_Listeners():
+def setup_button_listeners():
     btnList = document.querySelectorAll(".button")
     for i in range(len(btnList)):
         e = document.getElementById(btnList[i].id)
-        btn_event = create_proxy(Process_Button)
+        btn_event = create_proxy(process_button)
         e.addEventListener("click", btn_event)
 
 
@@ -32,33 +33,67 @@ def get_integer_partitions(n):
     return full_list
 
 
-async def Process_Button(event):
+def multiplicity_of_idx(i, array):
+    return array.count(array[i])
+
+
+def get_multiplicity_vector(array):
+    array.sort(reverse=True)
+    lamb = "("
+
+    # Using our above multiplicity function to generate the multiplicity vector
+    for i in range(len(array)):
+        if i == 0:
+            lamb += str(array[i]) + "^" + str(multiplicity_of_idx(i, array))
+        elif array[i] != array[i - 1]:
+            if i == len(array) - 1:
+                lamb += ", " + str(array[i]) + "^" + str(multiplicity_of_idx(i, array))
+            else:
+                lamb += ", " + str(array[i]) + "^" + str(multiplicity_of_idx(i, array))
+
+    lamb += ")"
+
+    return lamb
+
+
+async def process_button(event):
     if document.getElementById("evtMsg").innerHTML == '100':  # button plot_it
-        fig = await plot_it()
-        # pyscript.write('lineplot', fig)
+        fig = await display_output()
+        pyscript.write('user-IO', fig)
 
 
-async def plot_it(*args, **kwargs):
-    output_box = Element('test-output')
+async def display_output(*args, **kwargs):
 
-    text = Element('test-input').element.value
+    text = Element('input-1').element.value
 
-    str_arr = np.array(text.split(","))
+    text_arr = text.split(",")
     int_arr = []
 
-    for num in str_arr:
+    for num in text_arr:
         int_arr.append(int(num))
 
     int_arr.sort(reverse=True)
-    yd = ""
 
-    # We use the "☐" character to represent a block in the diagram
-    for num in int_arr:
-        for i in range(num):
-            yd += "☐"
-        yd += "\n"
+    rows = len(int_arr)
+    max_blocks = max(map(int, int_arr))
 
-    output_box.write(yd)
+    fig, ax = plt.subplots()
+
+    for row, num_blocks in enumerate(int_arr, 1):
+        num_blocks = int(num_blocks)
+        for i in range(max_blocks):
+            if i < num_blocks:
+                rect = patches.Rectangle((i, rows - row), 1, 1, linewidth=1, edgecolor='black', facecolor='blue')
+                ax.add_patch(rect)
+
+    ax.set_xlim(0, max_blocks)
+    ax.set_ylim(0, rows)
+    ax.set_aspect('equal')
+    plt.axis('off')
+    plt.title("Young Diagram for lambda = " + get_multiplicity_vector(int_arr))
+    # plt.show()
+    fig
+    return fig
 
 
-Setup_Button_Listeners()
+setup_button_listeners()
