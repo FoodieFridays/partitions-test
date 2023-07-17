aee573c976882408bfee9cf7741e04967ccfca8a
FoodieFridays <61214708+FoodieFridays@users.noreply.github.com>
2023-07-17 18:12:07 -0400

    KaTeX test in random_partition.py

--- random_partition.py
+++ random_partition.py
@@ -78,7 +78,8 @@ async def display_output(*args, **kwargs):
 
     lamb.sort(reverse=True)
 
-    output_box.write("Array Representation: \(" + str(lamb) + "\), Multiplicity Vector Representation: \(" + get_multiplicity_vector(lamb) + "\)")
+    # output_box.write("Array Representation: \(" + str(lamb) + "\), Multiplicity Vector Representation: \(" + get_multiplicity_vector(lamb) + "\)")
+    output_box.write("\( [1^1,2^2,3^3,4^4,5^5] \)")
     # return lamb
 
 
